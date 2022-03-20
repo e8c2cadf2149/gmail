3b94ba02aba2c7b3c25c6d773ae6559a2be42de2
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-03-20 20:28:24 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,12 @@
+import smtplib
+from email.mime.text import MIMEText
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login('testpyhton11@gmail.com', 'e8c2cadf2149')
+SUBJECT = 'This  Project is under development '
+TEXT = ' Testing.'
+
+message = f'Subject: {SUBJECT}\n\n{TEXT}'
+server.sendmail(f'testpyhton11@gmail.com', 'gautambisht709@gmail.com', message)
+server.quit()
