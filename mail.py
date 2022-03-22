dda09f9907b7abe3f7aed1173d471989b5a13eb4
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-03-22 07:33:32 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,7 +3,7 @@ import time
 
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login('testpyhton11@gmail.com', 'e8c2cadf2149')
+server.login('Your_Email', 'Your-Password')
 SUBJECT = 'This  Project is under development '
 s = time.strftime("%Y-%b-%d %I:%M")
 TEXT =  s, ' Testing for the parameters'
@@ -14,7 +14,7 @@ TEXT =  s, ' Testing for the parameters'
 
 message = f'Subject: {SUBJECT}\n\n{TEXT}'
 while True:
-    server.sendmail(f'testpyhton11@gmail.com', 'gautambisht709@gmail.com', message)
+    server.sendmail(f'testpyhton11@gmail.com', 'Reciever's_Email', message)
     print("Mail sent at :", s)
     time.sleep(1200)
 
